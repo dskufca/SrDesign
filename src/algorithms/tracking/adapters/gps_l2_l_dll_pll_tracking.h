db5b52daf1782cc54a4edd551b1c9cc8e0c0a495
Danielle Skufca <danielle.skufca@gmail.com>
2018-03-04 23:32:23 -0500

    add .h file for L2CL tracking

---
+++ src/algorithms/tracking/adapters/gps_l2_l_dll_pll_tracking.h
@@ -0,0 +1,20 @@
+// gps_l2_l_dll_pll_tracking.h
+// Danielle Skufca
+// Sr Design 2018
+
+// This is a header file for a class that implements a code DLL
+// and carrier PLL tracking loop 
+
+#ifndef GNSS_SDR_gps_l2_l_dll_pll_tracking_H_
+#define GNSS_SDR_gps_l2_l_dll_pll_tracking_H_
+
+#include <string>
+#include "tracking_interface.h"
+#include "gps_l2_l_dll_pll_tracking_cc.h"
+
+class ConfigurationInterface;
+
+
+
+
+#endif 
